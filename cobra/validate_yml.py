8e6e9db8760403399d18191674fd14da9f2c67b5
ryan g <mail.ryan.green@gmail.com>
2017-11-15 18:46:44 -0500

    working on making validation easier

---
+++ cobra/validate_yml.py
@@ -0,0 +1,11 @@
+from util import editor_utility as eu
+
+def validate(yml):
+    for label in eu.get_question_labels(yml):
+        eu.print_question_data(yml, label)
+    
+if __name__ == '__main__':
+    #files = ['enabled/2_basic_python.yml', 'enabled/3_files_paths.yml']
+    files = ['enabled/test.yml']
+    for file in files:
+        validate(file)
