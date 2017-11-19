163ae1c1950efd257aa912682b5ae3286c99fb4e
Dan Norris <norris.daniel@gmail.com>
2017-11-19 12:38:09 -0800

    working on modules workaround

--- 3-files_and_paths/problems.py
+++ 3-files_and_paths/problems.py
@@ -6,15 +6,15 @@ import re
 import shutil
 import string
 
-def _randStr(str_len=10):
-    return ''.join(random.choices(string.ascii_uppercase, k=str_len))
+def _randStr():
+    return ''.join(random.choices(string.ascii_uppercase, k=10))
 
 def moveAFile(src_path, dst_path):
     '''os_basics_1
     Write a function called moveAFile that accepts a source file path and a
     dest file path and moves the file to the destination.
-    >>> f0 = './{}'.format(_randStr())
-    >>> f1 = './{}'.format(_randStr())
+    >>> f0 = './{}'.format(''.join(random.choices(string.ascii_uppercase, k=10)))
+    >>> f1 = './{}'.format(''.join(random.choices(string.ascii_uppercase, k=10)))
     >>> content = 'asdf'
     >>> len(content) == open(f0, 'w').write(content)
     True
@@ -35,8 +35,8 @@ def cdAndWrite(dir_path, file_name, content):
     Write a function called cdAndWrite that changes directory to the given
     path, then edits a file and writes the given content to it. Return the
     size of the file in bytes! 
-    >>> f0 = './{}'.format(_randStr())
-    >>> d0 = './{}'.format(_randStr())
+    >>> f0 = './{}'.format(''.join(random.choices(string.ascii_uppercase, k=10)))
+    >>> d0 = './{}'.format(''.join(random.choices(string.ascii_uppercase, k=10)))
     >>> os.mkdir(d0)
     >>> cwd = os.path.realpath('.')
     >>> content = 'asdf'
@@ -61,8 +61,8 @@ def listDirFilter(dir_path, regex_filter):
     regex filter.  It retuns a list of files at the directory path whos names
     match the given regex.  Return only the file names, not their complete
     path. 
-    >>> e0 = _randStr()
-    >>> e1 = _randStr()
+    >>> e0 = ''.join(random.choices(string.ascii_uppercase, k=10))
+    >>> e1 = ''.join(random.choices(string.ascii_uppercase, k=10))
     >>> files = {e0: set(), e1: set()}
     >>> for e in (e0, e1):
     ...     for n in range(5):
@@ -131,8 +131,8 @@ def readFileToString(file_path):
     path, and returns the contents of the file as a string value. You can
     assume that you'll have access to the file, it won't be binary data, you
     don't need to do anything to the given path to open it.  
-    >>> f0 = './{}'.format(_randStr())
-    >>> c0 = '{}\\n{}\\n'.format(_randStr(), _randStr())
+    >>> f0 = './{}'.format(''.join(random.choices(string.ascii_uppercase, k=10)))
+    >>> c0 = '{}\\n{}\\n'.format(''.join(random.choices(string.ascii_uppercase, k=10)), ''.join(random.choices(string.ascii_uppercase, k=10)))
     >>> len(c0) == open(f0, 'w').write(c0)
     True
     >>> c0 == readFileToString(f0)
@@ -149,8 +149,8 @@ def readLinesFromFile(file_path):
     Write a function called readLinesFromFiles that accepts a path, reads the
     contents, and returns a list of stings for each line in the file. Do not
     include newline characters in the returned strings.
-    >>> f0 = './{}'.format(_randStr())
-    >>> c0 = "{}\\n{}\\n".format(_randStr(), _randStr())
+    >>> f0 = './{}'.format(''.join(random.choices(string.ascii_uppercase, k=10)))
+    >>> c0 = "{}\\n{}\\n".format(''.join(random.choices(string.ascii_uppercase, k=10)), ''.join(random.choices(string.ascii_uppercase, k=10)))
     >>> len(c0) == open(f0, 'w').write(c0)
     True
     >>> c0.split('\\n') == readLinesFromFile(f0)
@@ -169,9 +169,9 @@ def appendLineToFile(file_path, content):
     Write a function called appendLineToFile that accepts a path and a string,
     and writes the string to the end of the file (without modifying the
     existing contents in the file).
-    >>> f0 = './{}'.format(_randStr())
-    >>> c0 = "{}\\n{}\\n".format(_randStr(), _randStr())
-    >>> c1 = _randStr()
+    >>> f0 = './{}'.format(''.join(random.choices(string.ascii_uppercase, k=10)))
+    >>> c0 = "{}\\n{}\\n".format(''.join(random.choices(string.ascii_uppercase, k=10)), ''.join(random.choices(string.ascii_uppercase, k=10)))
+    >>> c1 = ''.join(random.choices(string.ascii_uppercase, k=10))
     >>> len(c0) == open(f0, 'w').write(c0)
     True
     >>> appendLineToFile(f0, c1)
@@ -189,9 +189,9 @@ def truncateWriteFile(file_path, content):
     Write a function called truncateWriteFile that accepts a path and a
     string, and writes the string to the file, truncating (overwriting) any
     existing content in the file. 
-    >>> f0 = './{}'.format(_randStr())
-    >>> c0 = "{}\\n{}\\n".format(_randStr(), _randStr())
-    >>> c1 = _randStr()
+    >>> f0 = './{}'.format(''.join(random.choices(string.ascii_uppercase, k=10)))
+    >>> c0 = "{}\\n{}\\n".format(''.join(random.choices(string.ascii_uppercase, k=10)), ''.join(random.choices(string.ascii_uppercase, k=10)))
+    >>> c1 = ''.join(random.choices(string.ascii_uppercase, k=10))
     >>> len(c0) == open(f0, 'w').write(c0)
     True
     >>> truncateWriteFile(f0, c1)
@@ -209,9 +209,9 @@ def rewriteInLower(source_path, dest_path):
     Write a function called rewriteInLower that accepts a source and
     destination path.  It reads in the source path, converts the contents to
     all lower case, and writes them to the destination path. 
-    >>> f0 = './{}'.format(_randStr())
-    >>> f1 = './{}'.format(_randStr())
-    >>> c0 = "!{}\\n{}13\\n! #@\\n".format(_randStr(), _randStr()).upper()
+    >>> f0 = './{}'.format(''.join(random.choices(string.ascii_uppercase, k=10)))
+    >>> f1 = './{}'.format(''.join(random.choices(string.ascii_uppercase, k=10)))
+    >>> c0 = "!{}\\n{}13\\n! #@\\n".format(''.join(random.choices(string.ascii_uppercase, k=10)), ''.join(random.choices(string.ascii_uppercase, k=10))).upper()
     >>> len(c0) == open(f0, 'w').write(c0)
     True
     >>> rewriteInLower(f0, f1)
